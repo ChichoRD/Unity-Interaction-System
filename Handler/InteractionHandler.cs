0b6d2367a6b3a08c5b959f6e4e8d9587a9f7d0db
ChichoRD <chichord2005@hotmail.com>
2023-12-17 05:03:15 +0100

    Default InteractionHandler evaluates arguments despite null interactor

--- Handler/InteractionHandler.cs
+++ Handler/InteractionHandler.cs
@@ -8,6 +8,10 @@ namespace InteractionSystem.Handler
     public class InteractionHandler : MonoBehaviour, IInteractionHandler
     {
         public void HandleInteraction<TInteractionRequestInfo, TInteractionResponse>(IInteractor<TInteractionResponse> interactor, IInteractable<TInteractionRequestInfo, TInteractionResponse> interactable, in IInteractionRequest<TInteractionRequestInfo> request)
-            where TInteractionResponse : IInteractionResponse => interactor?.TryInteract(interactable.TryInteract(request));
+            where TInteractionResponse : IInteractionResponse
+        {
+            TInteractionResponse response = interactable.TryInteract(request);
+            interactor?.TryInteract(response);
+        }
     }
 }
